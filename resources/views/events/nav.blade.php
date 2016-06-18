37bc0b6f6777e536e8ac0d072ab6dae4af8d88db
Raymond Ativie <raymond@reftek.co>
2016-06-19 00:37:06 +0100

    change nav home button to appropriate

--- resources/views/events/nav.blade.php
+++ resources/views/events/nav.blade.php
@@ -44,7 +44,7 @@
                             <!-- Collect the nav links, forms, and other content for toggling -->
                             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                                 <ul class="nav navbar-nav">
-                                    <li><a href="{{ url('home') }}">Home</a></li>
+                                    <li><a href="{{ url('events') }}">Home</a></li>
                                     <li><a href="{{ url('about') }}">About</a></li>
                                 </ul>
                                 <ul class="nav navbar-nav navbar-right">
