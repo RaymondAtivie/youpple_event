4c549e9212b84636c9c5bf66cd64598ca56429de
Douglas Kesi-Ayeba Kendyson <kendyson@kendyson.com>
2016-08-04 14:52:24 +0100

    function on how to save images

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -89,4 +89,76 @@ class AdminController extends Controller
         return view('admin.pages.feature.providers');
     }
 
+    ////
+    // public function addVendorprofile() {
+    //   $gallery = [];
+    //   $slim = Input::get('slim');
+    //
+    //   // $description =  Purifier::clean(Input::get('description'));
+    //   $description =  Input::get('description');
+    //   $images = Slim::getImages();
+    //
+    //   // dd(Input::all());
+    //   if ($images == false || $slim[0] == "") {
+    //     return response()->json(['result' => 'ferror','description' => 'Featured Image required']);
+    //   }else {
+    //     $rules = array(
+    //         'slim' => 'required|min:1',
+    //         'name' => 'required',
+    //         'description' => 'required',
+    //         'state' => 'required',
+    //         'category' => 'required',
+    //         'address' => 'required',
+    //         'contact' => 'required',
+    //         'pricerange' => 'required',
+    //         'mobile' => 'required',
+    //         'email' => 'required',
+    //         'travel' => 'required',
+    //
+    //     );
+    //     $redirect = url('myaccount/profiles');
+    //
+    //     $validator = Validator::make(Input::all(), $rules);
+    //     if ($validator->fails()){
+    //         return response()->json(['result' => 'valerror','details' => $validator->messages()], 200);
+    //     }else {
+    //       foreach ($images as $image) {
+    //         $file = Slim::saveFile($image['output']['data'],$image['input']['name'],$image['input']['ext'],NULL);
+    //         $gallery[] = ellipsis($file['name'],100);
+    //       }
+    //       $obj =  new Vendorprofile;
+    //       $obj->name = Input::get('name');
+    //       $slug = to_slug(Input::get('name'));
+    //       $obj->slug = $slug;
+    //       $code = generate_profile_code();
+    //       $obj->code = $code;
+    //
+    //       $obj->category_id = Input::get('category');
+    //       $obj->subcategory_ids = json_encode(Input::get('subcategory_ids'));
+    //       $obj->description = $description;
+    //       $obj->state_id = Input::get('state');
+    //       $obj->tagline = Input::get('tagline');
+    //       $obj->pricerange_id = Input::get('pricerange');
+    //       $obj->address = Input::get('address');
+    //       $obj->email = Input::get('email');
+    //       $obj->travel = Input::get('travel');
+    //       $obj->mobile = Input::get('mobile');
+    //       $obj->contact = Input::get('contact');
+    //       $obj->website = Input::get('website');
+    //       $obj->social = json_encode(Input::get('social'));
+    //       $obj->location = json_encode(Input::get('location'));
+    //       $obj->featured = $gallery[0];
+    //       $obj->gallery = json_encode($gallery);
+    //       $obj->user_id = Auth::user()->user_id;
+    //       $obj->save();
+    //       return response()->json(['result' => 'success','link' => $redirect]);
+    //
+    //       // dd(Input::all());
+    //     }
+    //   }
+    //   // dd($gallery);
+    //   // dd(Input::all());
+    // }
+
+
 }
